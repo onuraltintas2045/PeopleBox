87a6ea968472a12d454f55eda38bf26dec6d9aa7
onuraltintas2045 <onur.altintas2045@gmail.com>
2025-07-09 06:16:24 +0300

    User ve Favorite User data modelleri eklendi.

--- PeopleBox/Models/FavoriteUser.swift
+++ PeopleBox/Models/FavoriteUser.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class FavoriteUser: Identifiable {
+    @Attribute(.unique) var id: String
+    var fullName: String
+    var email: String
+    var age: Int
+    var phone: String
+    var location: String
+    var profileImageURL: String
+    var gender: String
+    var nationality: String
+
+    init(from user: User) {
+        self.id = user.id
+        self.fullName = user.fullName
+        self.email = user.email
+        self.age = user.age
+        self.phone = user.phone
+        self.location = user.location
+        self.profileImageURL = user.profileImageURL
+        self.gender = user.gender
+        self.nationality = user.nationality
+    }
+}
+
+extension FavoriteUser {
+    func toUser() -> User {
+        User(
+            id: self.id,
+            fullName: self.fullName,
+            email: self.email,
+            age: self.age,
+            phone: self.phone,
+            location: self.location,
+            profileImageURL: self.profileImageURL,
+            gender: self.gender,
+            nationality: self.nationality
+        )
+    }
+}
--- PeopleBox/Models/User.swift
+++ PeopleBox/Models/User.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+
+// MARK: - UI Model
+struct User: Identifiable, Equatable, Codable {
+    let id: String
+    let fullName: String
+    let email: String
+    let age: Int
+    let phone: String
+    let location: String
+    let profileImageURL: String
+    let gender: String
+    let nationality: String
+}
+
+// MARK: - API Mapping
+extension User {
+    // MARK: - User Api Response Model
+    struct UserResponse: Codable {
+        let gender: String
+        let name: Name
+        let location: Location
+        let email: String
+        let dob: DOB
+        let phone: String
+        let picture: Picture
+        let nat: String
+        let login: Login
+
+        // MARK: - Nested Models
+        struct Name: Codable {
+            let first: String
+            let last: String
+        }
+
+        struct Location: Codable {
+            let city: String
+            let state: String
+            let country: String
+        }
+
+        struct DOB: Codable {
+            let age: Int
+        }
+
+        struct Picture: Codable {
+            let large: String
+        }
+
+        struct Login: Codable {
+            let uuid: String
+        }
+    }
+
+    // MARK: - API Root Response
+    struct UserRootResponse: Codable {
+        let results: [UserResponse]
+    }
+}
