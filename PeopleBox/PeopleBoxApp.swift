9ac7f4d1a22b6d0ed6cb4475173e6a0d7e5f0dbc
onuraltintas2045 <onur.altintas2045@gmail.com>
2025-07-09 05:37:54 +0300

    Initial Commit

---
+++ PeopleBox/PeopleBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleBoxApp.swift
+//  PeopleBox
+//
+//  Created by Onur Altintas on 9.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct PeopleBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
